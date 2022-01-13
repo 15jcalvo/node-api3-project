74e9e886f202902fa9af8a09b942107805016db7
James_Calvo <15jcalvo@gmail.com>
2022-01-12 20:44:00 -0500

    added GET posts endpoint

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -27,37 +27,51 @@ router.get('/:id', logger, validateUserId, (req, res) => {
   res.json(req.user)
 });
 
-router.post('/', logger, validateUser, (req, res, next) => {
+router.post('/', logger, validateUser, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   try { const { id, name } = req.body
-  const newUser = Users.insert({ id, name })
+  const newUser = await Users.insert({ id, name })
   res.status(201).json(newUser)
   } catch(err) {
     next(err)
   }
 });
 
-router.put('/:id', logger, validateUserId, validateUser, (req, res, next) => {
+router.put('/:id', logger, validateUserId, validateUser, async (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   try { const { id, name } = req.body
-  const updatedUser = Users.update({ id, name })
+  const updatedUser = await Users.update({ id, name })
   res.status(200).json(updatedUser)
   } catch(err) {
     next(err)
   }
 });
 
-router.delete('/:id', logger, (req, res) => {
+router.delete('/:id', logger, validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  try{
+    const deletedUser = await Users.remove(req.params.id)
+    res.json(deletedUser)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.get('/:id/posts', logger, (req, res) => {
+router.get('/:id/posts', logger, validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  try{
+    const posts = await Users.getUserPosts(req.params.id)
+    if (posts.length > 0) {
+      res.status(200).json(posts)
+    } 
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.post('/:id/posts', logger, (req, res) => {
